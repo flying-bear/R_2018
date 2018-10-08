1184b50086c5466cbd75cb79c9c79e895f244352
Flying Bear <Galka1999@gmail.com>
2018-10-08 15:32:25 +0300

    Added data for CW4

---
+++ CW4.R
@@ -0,0 +1,3 @@
+library(tidyr)
+quartet <- subset(read.csv("https://gist.githubusercontent.com/ericbusboom/b2ac1d366c005cd2ed8c/raw/c92c66e43d144fa9c29dbd602d5af6988e8db533/anscombes.csv"), select = -id)
+datasaurus <- read_tsv('')
